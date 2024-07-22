9018f8d1b639fbc796ae3f20de56974ac474316b
fluxograma <anaelisaandrade65@gmail.com>
2024-07-22 11:12:16 -0300

    primeiro

---
+++ Flucograma_daequipe/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Flucograma_daequipe;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Fornecedor.cs
@@ -0,0 +1,23 @@
+public class Fornecedor
+{
+    string nome;
+    int id;
+    public void  SetNome ( string Nome)
+{
+    This nome = Nome; 
+}
+    public string GetNome()
+{
+        return nome; 
+} 
+    public void  SetId ( string Id)
+{ 
+    id = Id; 
+}
+    public string GetId()
+{
+    public void SetCNPJ ( string CNPJ)
+}
+
+
+}
